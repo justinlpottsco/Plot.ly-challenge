4301513bb219a718b4112debd75d0c9c61c3d5e1
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2021-01-10 18:01:34 -0700

    plot

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,34 +4,57 @@ d3.json('samples.json').then(function(data) {
     console.log(data);
   });
   
-  //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
     //Use sample_values as the values for the bar chart.
     //Use otu_ids as the labels for the bar chart.
     //Use otu_labels as the hovertext for the chart.
   
   
-  //   function init() {
-  //     // Use D3 to select the dropdown menu
-  //     var dropdownMenu = d3.selectAll("#selectOption").node();
-  //     // Assign the dropdown menu item ID to a variable
-  //     var dropdownMenuID = dropdownMenu.id;
-  //     // Assign the dropdown menu option to a variable
-  //     var selectedOption = dropdownMenu.value;
+function init() {
+// Use D3 to select the dropdown menu
+var dropdownMenu = d3.selectAll("#selectOption").node();
+// Assign the dropdown menu item ID to a variable
+var dropdownMenuID = dropdownMenu.id;
+// Assign the dropdown menu option to a variable
+var selectedOption = dropdownMenu.value;
+
+console.log(dropdownMenuID);
+console.log(selectedOption);
+}
+init();
+
+
+function dropdown(naveldata) {
+    naveldata['species'].forEach(name=>{
+        var newOption = d3.select('#sel')
+})
+
+
+function buildchart(sampledata) {
+    var graphDiv = document.getElementById('id_of_the_div')
+
+    var bardata = [{
+    x: [1999, 2000, 2001, 2002],
+    y: [10, 15, 13, 17],
+    type: 'bar',
+    orientation: 'horizontal'
+    }];
+
+    var barlayout = {
+    xaxis: {
+        title: 'Units of Bacteria Type(OTU)',
+        showgrid: false,
+        zeroline: false
+    },
+    yaxis: {
+        title: 'Bacteria Type(OTU)',
+        showline: false
+    }
+    };
     
-  //     console.log(dropdownMenuID);
-  //     console.log(selectedOption);
-  //   }
-    
-  // function dropdown(naveldata) {
-  //     naveldata['species'].forEach(name=>{
-  //         var newOption = d3.select('#sel')
-  //     })
-  // var data = [{
-  //     type: 'bar',
-  //     orientation: 'horizontal'
-  // }];
-  // };
-  
+    Plotly.newPlot(graphDiv, bardata, barlayout);
+};
+
   //Create a bubble chart that displays each sample.
     //Use otu_ids for the x values.
     //Use sample_values for the y values.
